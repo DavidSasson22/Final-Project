f76c64797b1bfef1e9678b6c76e3207891b1f123
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-05-02 20:32:33 +0300

    schema building

--- server/models/client.js
+++ server/models/client.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-// const validator = require('validator')
+const validator = require('validator');
 
 
 const clientSchme = mongoose.model('clients-collection', {
@@ -8,6 +8,7 @@ const clientSchme = mongoose.model('clients-collection', {
     required: true,
     minLength: 3,
     maxLength: 20,
+    trim: true,
   },
 
   lastName: {
@@ -15,6 +16,24 @@ const clientSchme = mongoose.model('clients-collection', {
     required: true,
     minLength: 3,
     maxLength: 20,
+    trim: true,
+  },
+
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if(validator.isEmail(!value)) {
+        throw new Error (`Email is invalid`)
+      }
+    }
+  },
+
+  joinedAt: {
+    type: Date,
+    default: Date.now,
   },
 
   isActive: {
